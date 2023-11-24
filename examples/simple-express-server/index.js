cf7b89735b9a6cfafd2cf690a7f4b735f4f141e9
Mitchell Vera <mitchell.vera@usach.cl>
2023-11-24 01:05:14 -0300

    Change in how metrics are selected
    Instead of a pre-defined list of metrics, the metrics to take into consideration are defined on the client side and are part of the request to the StArE.js server. If no metrics are provided then the older pre-defined list of metrics will be taken into consideration.

--- examples/simple-express-server/index.js
+++ examples/simple-express-server/index.js
@@ -8,7 +8,6 @@ const figlet = require('figlet');
 
 app.use(cors());
 app.options('*', cors());
-
 const myMetrics = {
   a: require('./my-metrics/a'),
   b: require('./my-metrics/b')
@@ -19,7 +18,7 @@ const mySERPs = {
 };
 
 const stare = require('../..')({
-  engines: ['elasticsearch', 'solr'],
+  engines: ['elasticsearch', 'solr','searchcloud', 'bing'],
   // engines: ['bing', 'ecosia', 'google', 'searchcloud', 'personalSERP'],
   // personalMetrics: myMetrics,
   // personalSERPs: mySERPs,
@@ -27,9 +26,10 @@ const stare = require('../..')({
   //   apiKey: process.env.GOOGLE_API_KEY,
   //   apiCx: process.env.GOOGLE_API_CX
   // },
-  // bing: {
-  //   serviceKey: process.env.BING_SERVICE_KEY
-  // },
+   bing: {
+     serviceKey: process.env.BING_SERVICE_KEY,
+
+   },
   elasticsearch: {
     baseUrl: 'http://143.110.239.29',
     _index: 'movies',
@@ -51,10 +51,28 @@ const stare = require('../..')({
 
 app.get('/:engine', (request, response) => {
   let engine = request.params.engine;
-  let { query, numberOfResults } = request.query;
+  let { query, numberOfResults, metrics } = request.query;
 
   // let metrics = [];
-  let metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
+  //let metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
+    if (metrics) {
+    metrics = metrics.split(',');
+  } else {
+    // Default metrics if not provided
+    metrics = ['keywords-position', 'language', 'length', 'links', 'multimedia', 'perspicuity', 'ranking'];
+  }
+
+  //Checked input metrics
+  
+  console.log("List of input metrics:");
+  console.log("-------------------");
+  metrics.forEach(element => {
+  console.log(element);
+  });
+  console.log("==================");
+
+
+
   stare(engine, query, numberOfResults, metrics)
     .then(result => response.status(200).json(result))
     .catch(err => response.status(500).json(err));
